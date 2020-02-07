e2bf504596c5606d276930b619891701e5565a6d
Lillian Luzinsky <eyeris@tpg.com.au>
2020-02-07 13:28:00 +1100

    Displaying column title at this stage

--- src/initial-data.js
+++ src/initial-data.js
@@ -1,3 +1,4 @@
+
 const initialData = {
   tasks: {
     'task-1': { id: 'task-1', content: 'Task 1'},
@@ -9,11 +10,11 @@ const initialData = {
     'column-1': {
       id: 'column-1',
       title: 'To do',
-      taskIds: ['task-1', 'task-2', 'task-3', 'task-4']
-    }
+      taskIds: ['task-1', 'task-2', 'task-3', 'task-4'],
+    },
   },
 
-  columnOrder: ['colunm-1'],
+  columnOrder: ['column-1'],
 };
 
 export default initialData;
