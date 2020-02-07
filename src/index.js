3d0c47fcd6f150b69c3b893624e3b7f8b1747c92
Lillian Luzinsky <eyeris@tpg.com.au>
2020-02-07 14:29:07 +1100

    adding draggable feature

--- src/index.js
+++ src/index.js
@@ -7,22 +7,23 @@ import Column from './column';
 class App extends React.Component {
   state = initialData;
 
+  onDragEnd = result =>{
+
+  }
+
   render() {
 
     return(
-      <DragDropContext>
-        onDragStart
-        onDragUpdate
-        onDragEnd
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        {this.state.columnOrder.map(columnId => {
+          const column = this.state.columns[columnId];
+          const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+
+          return <Column key={column.id} column={column} tasks={tasks}/>
+        })}
       </DragDropContext>
     )
-    return this.state.columnOrder.map(columnId =>{
-      const column = this.state.columns[columnId];
-      const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
 
-      return <Column key={column.id} column={column} tasks={tasks} />;
-
-    });
 
   }
 }
